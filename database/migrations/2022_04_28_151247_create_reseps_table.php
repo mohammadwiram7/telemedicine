1691d387d38426396fdfadc2cca035e675272840
Mohammad Wira M <mohammadwiram7@gmail.com>
2022-04-30 14:15:19 +0700

    Create obat resep model controller

---
+++ database/migrations/2022_04_28_151247_create_reseps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reseps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('dokter_id');
+            $table->foreignId('pasien_id');
+            $table->foreignId('diagnosa_id');
+            $table->foreignId('obat_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reseps');
+    }
+}
